217b946186d6f1a03efcacd0579a168ff22de9f7
k2marcos <marcosfeitosapalmas@gmail.com>
2024-05-08 10:51:32 -0300

    first Comit

---
+++ src/br/tec/marcos/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.tec.marcos.principal;
+
+import br.tec.marcos.Funcao.Funcoes;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Funcoes func = new Funcoes();
+		
+		func.mediaAritimetica();
+		
+		
+	}
+}
